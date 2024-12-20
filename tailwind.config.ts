dc2a28cfd9fd11058ad3f02464ac2850791d82fa
jaskamal2724 <jaskamalsingh7872@gmail.com>
2024-12-20 23:11:34 +0530

    featured courses

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ export default {
     },
     animation: {
       spotlight: "spotlight 2s ease .75s 1 forwards",
+      shimmer: "shimmer 2s linear infinite",
     },
     keyframes: {
       spotlight: {
@@ -31,6 +32,14 @@ export default {
           opacity: "1",
           transform: "translate(-50%,-40%) scale(1)",
         },
+      },
+      shimmer:{
+        from:{
+          backgroundPosition: "0 0",
+        },
+        to:{
+          backgroundPosition: "-200% 0",
+        }
       },
     },
   },
